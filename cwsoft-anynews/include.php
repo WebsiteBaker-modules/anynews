d5fa70eb4ee787f0547e71de9e4b8beb86b35142
Christian Sommer <christian.sommer@gmx.ch>
2013-02-09 18:56:09 +0100

    Implemented more flexible parameter handling to Anynews
    - added getNewsItems($options) which returns string with parsed template
    - Deprecated: displayNewsItems(), serves as legacy wrapper for old releases

--- cwsoft-anynews/include.php
+++ cwsoft-anynews/include.php
@@ -23,25 +23,42 @@ if (defined('WB_PATH') == false) {
 	exit("Cannot access this file directly");
 }
 
-// function to display news items on every page via (invoke function from template or code page)
-if (! function_exists('displayNewsItems')) {
-	function displayNewsItems(
-		$group_id = 0,                  // IDs of news to show, matching defined $group_id_type (default:=0, all news, 0..N, or array(2,4,5,N) to limit news to IDs matching $group_id_type)
-		$max_news_items = 10,           // maximal number of news shown (default:= 10, min:=1, max:= 999)
-		$max_news_length = -1,          // maximal length of the short news text shown (default:=-1 => full news length)
-		$display_mode = 1,              // 1:=details (default); 2:=list; 3:=coda-slider; 4:flexslider; 4-98 (custom template: display_mode_X.htt); 99:=cheat sheet
-		$lang_id = 'AUTO',              // language file to load and lang_id used if $lang_filer = true (default:= auto, examples: AUTO, DE, EN)
-		$strip_tags = true,             // true:=remove tags from short and long text (default:=true); false:=don´t strip tags
-		$allowed_tags = '<p><a><img>',  // tags not striped off (default:='<p><a><img>')
-		$custom_placeholder = false,    // false:= none (default), array('MY_VAR_1' => '%TAG%#', ... 'MY_VAR_N' => '#regex_N#' ...)
-		$sort_by = 1,                   // 1:=position (default), 2:=posted_when, 3:=published_when, 4:=random order, 5:=number of comments
-		$sort_order = 1,                // 1:=descending (default), 2:=ascending
-		$not_older_than = 0,            // 0:=disabled (default), 0-999 (only show news `published_when` date <=x days; 12 hours:=0.5)
-		$group_id_type = 'group_id',    // type used by group_id to extract news entries (supported: 'group_id', 'page_id', 'section_id', 'post_id')
-		$lang_filter = false            // flag to enable language filter (default:= false, show only news from a news page, which language fits $lang_id)
-  	)
+/**
+ * Extracts requested news from the WebsiteBaker news module
+ * and returns a string with the parsed cwsoft-anynews template.
+ */
+if (! function_exists('getNewsItems')) {
+	function getNewsItems($options=array())
 	{
 		global $wb, $database, $LANG;
+
+		// default settings
+		$defaults = array(
+			'group_id' => 0,                  // IDs of news to show, matching defined $group_id_type (default:=0, all news, 0..N, or array(2,4,5,N) to limit news to IDs matching $group_id_type)
+			'max_news_items' => 10,           // maximal number of news shown (default:= 10, min:=1, max:= 999)
+			'max_news_length' => -1,          // maximal length of the short news text shown (default:=-1 => full news length)
+			'display_mode' => 1,              // 1:=details (default); 2:=list; 3:=coda-slider; 4:flexslider; 4-98 (custom template: display_mode_X.htt); 99:=cheat sheet
+			'lang_id' => 'AUTO',              // language file to load and lang_id used if $lang_filer = true (default:= auto, examples: AUTO, DE, EN)
+			'strip_tags' => true,             // true:=remove tags from short and long text (default:=true); false:=don´t strip tags
+			'allowed_tags' => '<p><a><img>',  // tags not striped off (default:='<p><a><img>')
+			'custom_placeholder' => false,    // false:= none (default), array('MY_VAR_1' => '%TAG%#', ... 'MY_VAR_N' => '#regex_N#' ...)
+			'sort_by' => 1,                   // 1:=position (default), 2:=posted_when, 3:=published_when, 4:=random order, 5:=number of comments
+			'sort_order' => 1,                // 1:=descending (default), 2:=ascending
+			'not_older_than' => 0,            // 0:=disabled (default), 0-999 (only show news `published_when` date <=x days; 12 hours:=0.5)
+			'group_id_type' => 'group_id',    // type used by group_id to extract news entries (supported: 'group_id', 'page_id', 'section_id', 'post_id')
+			'lang_filter' => false            // flag to enable language filter (default:= false, show only news from a news page, which language fits $lang_id)
+		);
+
+		// merge defaults and options array and remove unsupported keys
+		$settings = array_merge($defaults, $options);
+		foreach($settings as $key => $value) {
+			if (! array_key_exists($key, $defaults)) {
+				unset($settings[$key]);
+			}
+		}
+
+		// export variables into function scope
+		extract($settings);
 
 		/**
 		 * Include required Anynews files and language files
@@ -249,7 +266,52 @@ if (! function_exists('displayNewsItems')) {
 			}
 		}
 	
-		// ouput the final template
-		$tpl->display($data);
+		// return parsed template
+		return $tpl->render($data);
+	}
+}
+
+/**
+ * DEPRECATED: Serves as legacy wrapper for older cwsoft-anynews releases
+ * BETTER USE: echo getNewsItems($options) in new projects
+ * Passes over parameters to getNewsItems() and echos return string to screen
+ *
+ */
+if (! function_exists('displayNewsItmes')) {
+	function displayNewsItems(
+		$group_id = 0,                  // IDs of news to show, matching defined $group_id_type (default:=0, all news, 0..N, or array(2,4,5,N) to limit news to IDs matching $group_id_type)
+		$max_news_items = 10,           // maximal number of news shown (default:= 10, min:=1, max:= 999)
+		$max_news_length = -1,          // maximal length of the short news text shown (default:=-1 => full news length)
+		$display_mode = 1,              // 1:=details (default); 2:=list; 3:=coda-slider; 4:flexslider; 4-98 (custom template: display_mode_X.htt); 99:=cheat sheet
+		$lang_id = 'AUTO',              // language file to load and lang_id used if $lang_filer = true (default:= auto, examples: AUTO, DE, EN)
+		$strip_tags = true,             // true:=remove tags from short and long text (default:=true); false:=don´t strip tags
+		$allowed_tags = '<p><a><img>',  // tags not striped off (default:='<p><a><img>')
+		$custom_placeholder = false,    // false:= none (default), array('MY_VAR_1' => '%TAG%#', ... 'MY_VAR_N' => '#regex_N#' ...)
+		$sort_by = 1,                   // 1:=position (default), 2:=posted_when, 3:=published_when, 4:=random order, 5:=number of comments
+		$sort_order = 1,                // 1:=descending (default), 2:=ascending
+		$not_older_than = 0,            // 0:=disabled (default), 0-999 (only show news `published_when` date <=x days; 12 hours:=0.5)
+		$group_id_type = 'group_id',    // type used by group_id to extract news entries (supported: 'group_id', 'page_id', 'section_id', 'post_id')
+		$lang_filter = false            // flag to enable language filter (default:= false, show only news from a news page, which language fits $lang_id)
+	)
+	{
+		// get cwsoft-anynews output for given parameters
+		$output = getNewsItems(
+			$options = array(
+				'group_id' => $group_id,
+				'max_news_items' => $max_news_items,
+				'max_news_length' => $max_news_length,
+				'display_mode' => $display_mode,
+				'lang_id' => $lang_id,
+				'strip_tags' => $strip_tags,
+				'allowed_tags' => $allowed_tags,
+				'custom_placeholder' => $custom_placeholder,
+				'sort_by' => $sort_by,
+				'sort_order' => $sort_order,
+				'not_older_than' => $not_older_than,
+				'group_id_type' => $group_id_type,
+				'lang_filter' => $lang_filter
+			)
+		);
+		echo $output;
 	}
 }
